ad31e1fc6d85f8142d7974360aa5ae99bf0be717
VirtualEstatePlanner <VirtualEstatePlanner@users.noreply.github.com>
2019-07-18 02:13:24 -0400

    Update kill-vpn.sh

--- kill-vpn.sh
+++ kill-vpn.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-docker stop ikevpn
-docker rm ikevpn
+echo -n "Are you sure you want to destroy the VPN AND delete all mobileconfig files in this directory?"
+echo -n "THIS CANNOT BE UNDONE. (y/n)"
+
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+        echo "removing all mobileconfig files from this directory"
+        rm *.mobileconfig
+        echo "stopping vpn container"
+        docker stop ikevpn
+        echo "removing vpn container"
+        docker rm ikevpn
+        echo "Done"
+else
+    exit
+fi
