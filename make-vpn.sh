cef2baf5a7af9732e48c500f0f509bf291e81ece
VirtualEstatePlanner <VirtualEstatePlanner@users.noreply.github.com>
2019-07-18 02:25:51 -0400

    Update make-vpn.sh

--- make-vpn.sh
+++ make-vpn.sh
@@ -16,5 +16,6 @@ profile=$TEMPGLOBAL
 get_value "name for your .mobileconfig file"
 filename=$TEMPGLOBAL
 TEMPGLOBAL=''
+docker pull georgegeorgulasiv/ikevpn
 docker run --privileged -d --name ikevpn --restart=always -p 500:500/udp -p 4500:4500/udp georgegeorgulasiv/ikevpn
 docker run --privileged --rm --volumes-from ikevpn -e "HOST=$vpnhost" -e "CONN_NAME=$connection" -e "PROFILE_NAME=$profile" georgegeorgulasiv/ikevpn make-config.sh > $filename.mobileconfig
